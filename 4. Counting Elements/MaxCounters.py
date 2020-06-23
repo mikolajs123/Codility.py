972947be0bdbec5787deec05df8d5a6c104cf984
mikolajs123 <53497782+mikolajs123@users.noreply.github.com>
2020-06-23 11:02:07 +0200

    Update MaxCounters.py

--- 4. Counting Elements/MaxCounters.py
+++ 4. Counting Elements/MaxCounters.py
@@ -67,3 +67,26 @@ def solution1(N, A):
             max_1 = max_2
     counters = [max(max_1, i) for i in counters]    
     return counters
+
+def solution2(N, A):
+    counters = [0] * N
+    
+    start_line = 0
+    current_max = 0
+    
+    for i in A:
+        x = i - 1
+        if i > N:
+            start_line = current_max
+        elif counters[x] < start_line:
+            counters[x] = start_line + 1
+        else:
+            counters[x] += 1
+        if i <= N and counters[x] > current_max:
+            current_max = counters[x]
+    
+    for i in range(0, len(counters)):
+        if counters[i] < start_line:
+            counters[i] = start_line
+
+    return counters
